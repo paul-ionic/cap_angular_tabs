969ead6c92030854c7a94a872cffcf64415a75db
Paul LaCaille <paul@ionic.io>
2022-02-08 14:12:44 -0600

    move child from AppRoutingModule to Tab1PageRoutingModule

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -20,6 +20,10 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'child',
+        loadChildren: () => import('../child/child.module').then( m => m.ChildPageModule)
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
